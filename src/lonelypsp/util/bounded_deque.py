01df86ee4a76a7378ab0ee1442ac3166f8de909f
Timothy Moore <mtimothy984@gmail.com>
2024-12-19 11:37:01 -0800

    Add drainable version of asyncio.Queue

--- src/lonelypsp/util/bounded_deque.py
+++ src/lonelypsp/util/bounded_deque.py
@@ -1,13 +1,3 @@
-"""This doesn't really make sense in this library except that it's not
-a completely trivial implementation but it is necessary to implement the
-desired semantics for both lonelypss and lonelypsc, which both use lonelypsp,
-making this a convenient place to put the implementation. Alternatively,
-could duplicate this to both spots or make a utils library and put that in
-pypi and depend on it for both, which isn't great, or make a really specific
-library for pypi (e.g., "bounded_deque"), which will lead to the same problem
-if we need another collection
-"""
-
 from typing import Generic, Iterable, List, Optional, TypeVar
 
 
