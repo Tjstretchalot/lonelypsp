8f3ad7e480f5db9ae809c17a0a80996c71d7fb85
Timothy Moore <mtimothy984@gmail.com>
2025-01-22 08:35:41 -0800

    Add assert_never compat export

--- src/lonelypsp/compat.py
+++ src/lonelypsp/compat.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass as fast_dataclass
 from functools import partial
 from typing import TypeVar
 
-T = TypeVar("T")
+__all__ = ["fast_dataclass", "assert_never"]
 
+T = TypeVar("T")
 if sys.version_info >= (3, 10):
 
     def make_dataclass_fast(x: T) -> T:
@@ -17,3 +18,13 @@ else:
         return partial(x, frozen=True)  # type: ignore
 
     fast_dataclass = make_dataclass_fast(fast_dataclass)
+
+
+if sys.version_info >= (3, 11):
+    from typing import assert_never
+else:
+    from typing import NoReturn
+    from typing import NoReturn as Never
+
+    def assert_never(value: Never) -> NoReturn:
+        raise AssertionError(f"Unhandled type: {value!r}")
